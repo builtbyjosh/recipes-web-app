9bd9ef1b99c38a732e00c8f324f7038f71dac678
builtbyjosh <joshuacollins912@gmail.com>
2023-04-19 10:39:59 -0500

    configure crud operations

--- src/App.js
+++ src/App.js
@@ -1,18 +1,209 @@
-import { useState } from "react";
+import { useState, useEffect, startTransition } from "react";
 import FirebaseAuthService from "./FirebaseAuthService";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
+import AddEditRecipeForm from "./components/AddEditRecipeForm";
+import FirebaseFirestoreService from "./FirebaseFirestoreService";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [currentRecipe, setCurrentRecipe] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetchRecipes()
+      .then((fetchedRecipes) => {
+        setRecipes(fetchedRecipes);
+      })
+      .catch((error) => {
+        console.error(error.message);
+        throw error;
+      });
+  }, [user]);
 
   FirebaseAuthService.subscribeToAuthChanges(setUser);
+
+  const fetchRecipes = async () => {
+    const queries = [];
+    if (!user) {
+      queries.push({
+        field: "isPublished",
+        condition: "==",
+        value: true,
+      });
+    }
+    let fetchedRecipes = [];
+    try {
+      const response = await FirebaseFirestoreService.readDocuments({
+        collection: "recipes",
+        queries: queries,
+      });
+      const newRecipes = response.docs.map((recipeDoc) => {
+        const id = recipeDoc.id;
+        const data = recipeDoc.data();
+        data.publishDate = new Date(data.publishDate.seconds * 1000);
+
+        return { ...data, id };
+      });
+      fetchedRecipes = [...newRecipes];
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+    return fetchedRecipes;
+  };
+
+  const handleFetchRecipes = async () => {
+    try {
+      const fetchedRecipes = await fetchRecipes();
+      setRecipes(fetchedRecipes);
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+  };
+
+  const handleAddRecipe = async (newRecipe) => {
+    try {
+      const res = await FirebaseFirestoreService.createDocument(
+        "recipes",
+        newRecipe
+      );
+
+      handleFetchRecipes();
+
+      alert(`succesfully created a recipe with an ID of ${res.id}`);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleUpdateRecipe = async (newRecipe, recipeId) => {
+    try {
+      await FirebaseFirestoreService.updateDocument(
+        "recipes",
+        recipeId,
+        newRecipe
+      );
+      handleFetchRecipes();
+      alert(`successfully updated a recipe with ID = ${recipeId}`);
+      startTransition(() => {
+        setCurrentRecipe(null);
+      });
+    } catch (error) {
+      alert(error.message);
+      throw error;
+    }
+  };
+
+  const handleDeleteRecipe = async (recipeId) => {
+    const deleteConfirmation = window.confirm(
+      "Are you sure you want to delete this recipe?"
+    );
+    if (deleteConfirmation) {
+      try {
+        await FirebaseFirestoreService.deleteDocument("recipes", recipeId);
+        handleFetchRecipes();
+        startTransition(() => {
+          setCurrentRecipe(null);
+        });
+        window.scrollTo(0, 0);
+        alert(`successfully deleted a recipe with ID = ${recipeId}`);
+      } catch (error) {
+        alert(error.message);
+        throw error;
+      }
+    }
+  };
+
+  const handleEditRecipeClick = (recipeID) => {
+    const selectedRecipe = recipes.find((recipe) => {
+      return recipe.id === recipeID;
+    });
+    if (selectedRecipe) {
+      startTransition(() => {
+        setCurrentRecipe(selectedRecipe);
+      });
+      window.scrollTo(0, document.body.scrollHeight);
+    }
+  };
+
+  const handleEditRecipeCancel = () => {
+    startTransition(() => {
+      setCurrentRecipe(null);
+    });
+  };
+
+  const lookupCategoryLabel = (categoryKey) => {
+    const categories = {
+      breadsSandwichesAndPizza: "Breads, Sandwiches, and Pizza",
+      eggsAndBreakfast: "Eggs & Breakfast",
+      dessertsAndBakedGoods: "Desserts and Baked Goods",
+      fishAndSeafood: "Fish and Seafood",
+      vegetables: "Vegetables",
+    };
+    const label = categories[categoryKey];
+    return label;
+  };
+
+  const formatDate = (date) => {
+    const day = date.getUTCDate();
+    const month = date.getUTCMonth() + 1;
+    const year = date.getFullYear();
+
+    const dateString = `${month}-${day}-${year}`;
+    return dateString;
+  };
 
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Recipes App</h1>
         <LoginForm existingUser={user} />
+      </div>
+      <div className="main">
+        <div className="center">
+          <div className="recipe-list-box">
+            {recipes && recipes.length > 0 ? (
+              <div className="recipe-list">
+                {recipes.map((recipe) => {
+                  return (
+                    <div className="recipe-card" key={recipe.key}>
+                      {!recipe.isPublished && (
+                        <div className="unpublished">UNPUBLISHED</div>
+                      )}
+                      <div className="recipe-name">{recipe.name}</div>
+                      <div className="recipe-field">
+                        Category: {lookupCategoryLabel(recipe.category)}
+                      </div>
+                      <div className="recipe-field">
+                        Publish Date: {formatDate(recipe.publishDate)}
+                      </div>
+                      {user && (
+                        <button
+                          className="primary-button edit-button"
+                          type="button"
+                          onClick={() => handleEditRecipeClick(recipe.id)}
+                        >
+                          EDIT
+                        </button>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            ) : null}
+          </div>
+        </div>
+        {user && (
+          <AddEditRecipeForm
+            handleAddRecipe={handleAddRecipe}
+            handleUpdateRecipe={handleUpdateRecipe}
+            existingRecipe={currentRecipe}
+            handleEditRecipeCancel={handleEditRecipeCancel}
+            handleDeleteRecipe={handleDeleteRecipe}
+          />
+        )}
       </div>
     </div>
   );
